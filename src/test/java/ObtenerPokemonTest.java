15decb36ff4f57690a295f31d3bee4ff0a6b584f
Gonzalo <gonzalojimeno@gmail.com>
2022-04-27 10:36:23 +0200

    Clase 27/04/2022 UnitTestingPokemon

--- src/test/java/ObtenerPokemonTest.java
+++ src/test/java/ObtenerPokemonTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ObtenerPokemonTest {
@@ -14,26 +15,40 @@ public class ObtenerPokemonTest {
     @Test
     public void probarPokemon3Nombre(){
         // TODO Comprueba que al preguntar por Pokémon con id 3 obtienes a venasaur.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get(3);
+        Assertions.assertEquals(pokemon3.getName(), "venusaur");
     }
 
     @Test
-    public void probarPokemon3Id(){
-        // TODO Comprueba que al preguntar por Pokémon con venasaur obtienes el id 2.
+    public void probarPokemon3Id() {
+        // TODO Comprueba que al preguntar por Pokémon con venasaur obtienes el id 3.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get("venusaur");
+        Assertions.assertEquals(pokemon3.getId(), 3);
     }
-
     @Test
     public void probarPokemon3Peso(){
         // TODO Comprueba que al preguntar por Pokémon con 3 obtienes el peso de 1000.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get("venusaur");
+        Assertions.assertEquals(pokemon3.getWeight(), 1000);
     }
 
     @Test
     public void probarPokemon3Altura(){
         // TODO Comprueba que al preguntar por Pokémon con 3 obtienes la altura de 20.
+        Pokemon pokemon3 = ObtenerPokemonRequest.Companion.get("venusaur");
+        Assertions.assertEquals(pokemon3.getHeight(), 20);
     }
 
     @Test
     public void probarPokemon4a6(){
         // TODO Comprueba que los Pokémon (id = 4, id = 5, id = 6) obtienes a charmander, charmeleon y charizard.
+        Pokemon pokemon4 = ObtenerPokemonRequest.Companion.get("charmander");
+        Pokemon pokemon5 = ObtenerPokemonRequest.Companion.get("charmeleon");
+        Pokemon pokemon6 = ObtenerPokemonRequest.Companion.get("charizard");
+        Assertions.assertEquals(pokemon4.getName(), "charmander");
+        Assertions.assertEquals(pokemon5.getName(), "charmeleon");
+        Assertions.assertEquals(pokemon6.getName(), "charizard");
+
     }
 
     @Test
